76b26be272663802cb07e029dcf28705e42ea897
Ge Feng <ge.feng@Ges-MacBook-Pro.local>
2022-01-10 02:45:21 +0000

    goodbye 2021

---
+++ src/codeforces/goodbye2021/B.java
@@ -0,0 +1,129 @@
+package codeforces.goodbye2021;
+
+import java.io.*;
+import java.util.*;
+
+public class B {
+    void go() {
+        int n = Reader.nextInt();
+        String s = Reader.next();
+
+        if(n == 1) {
+            Writer.println(s + s);
+            return;
+        }
+
+        if(s.charAt(0) == s.charAt(1)) {
+            Writer.println(s.substring(0, 2));
+            return;
+        }
+
+        char pre = s.charAt(0);
+        int k = -1;
+        for(int i = 1; i < n; i++) {
+            if(s.charAt(i) > pre) {
+                k = i;
+                break;
+            }
+            pre = s.charAt(i);
+        }
+        k = k == -1 ? n : k;
+        String half = s.substring(0, k);
+        String mirror = new StringBuilder(half).reverse().toString();
+        Writer.println(half + mirror);
+    }
+    void solve() {
+        for(int T = Reader.nextInt(); T > 0; T--) go();
+    }
+    void run() throws Exception {
+        Reader.init(System.in);
+        Writer.init(System.out);
+        solve();
+        Writer.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new B().run();
+    }
+
+    public static class Reader {
+        public static StringTokenizer st;
+        public static BufferedReader br;
+
+        public static void init(InputStream in) {
+            br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+
+        public static String next() {
+            while(!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public static int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public static long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public static double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static class Writer {
+        public static PrintWriter pw;
+
+        public static void init(OutputStream os) {
+            pw = new PrintWriter(new BufferedOutputStream(os));
+        }
+
+        public static void print(String s) {
+            pw.print(s);
+        }
+
+        public static void print(char c) {
+            pw.print(c);
+        }
+
+        public static void print(int x) {
+            pw.print(x);
+        }
+
+        public static void print(long x) {
+            pw.print(x);
+        }
+
+        public static void println(String s) {
+            pw.println(s);
+        }
+
+        public static void println(char c) {
+            pw.println(c);
+        }
+
+        public static void println(int x) {
+            pw.println(x);
+        }
+
+        public static void flush() {
+            pw.flush();
+        }
+
+        public static void println(long x) {
+            pw.println(x);
+        }
+
+        public static void close() {
+            pw.close();
+        }
+    }
+}
