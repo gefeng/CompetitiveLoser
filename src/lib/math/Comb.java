d8477ce2e851bc6bb20e8976a6278bb5bfd26105
Ge Feng <ge.feng@Ges-MacBook-Pro.local>
2022-06-23 11:02:25 +0100

    add lib folder

--- src/lib/math/Comb.java
+++ src/lib/math/Comb.java
@@ -6,29 +6,37 @@ package lib.math;
 public class Comb {
     int MAX;
     long MOD;
-    long[] frac;
+    long[] fact;
     long[] invf;
     Comb(int n, long mod) {
         MAX = n;
         MOD = mod;
-        frac = new long[MAX + 1];
+        fact = new long[MAX + 1];
         invf = new long[MAX + 1];
 
-        frac[0] = 1;
+        fact[0] = 1;
         invf[0] = 1;
 
         for(int i = 1; i <= MAX; i++) {
-            frac[i] = frac[i - 1] * i % MOD;
+            fact[i] = fact[i - 1] * i % MOD;
         }
 
-        invf[MAX] = power(frac[MAX], MOD - 2);
+        invf[MAX] = power(fact[MAX], MOD - 2);
 
         for(int i = MAX - 1; i >= 0; i--) {
             invf[i] = invf[i + 1] * (i + 1) % MOD;
         }
     }
 
-    private long power(long x, long y) {
+    long get(int n, int k) {
+        if(n < 0 || k < 0 || n < k) {
+            return 0;
+        }
+
+        return fact[n] * (invf[k] * invf[n - k] % MOD) % MOD;
+    }
+
+    long power(long x, long y) {
         long res = 1;
         while(y > 0) {
             if(y % 2 == 1) {
@@ -40,6 +48,3 @@ public class Comb {
         return res;
     }
 }
-
-// Comb comb = new Comb(max_n)
-// comb.bc(n, k)
